7bf7be101dc1ebdb146235138ff13f76a49ad3c6
vezz <mati.majewski@wp.pl>
2022-10-16 20:58:16 +0200

    modified Dockerfile, modified api/axios

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16 AS Production
+FROM node:16
 
 ENV NODE_ENV=production
 
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+const url = process.env.REACT_APP_API_URL || 'http://127.0.0.1:4000/api';
 
-const baseURL = 'http://127.0.0.1:4000/api';
+const baseURL = url;
 
 export default axios.create({
-  baseURL: baseURL,
+  baseURL,
   withCredentials: true,
 });
 
